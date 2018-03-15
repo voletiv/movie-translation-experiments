72a7f31e6afa6c63610b5fe6f8ba58d42d26bd61
Vikram Voleti <vikky2904@gmail.com>
2018-03-15 23:30:48 +0530

    Different files for local and atom

--- exchange_dialogues/exchange_dialogues.py
+++ exchange_dialogues/exchange_dialogues.py
@@ -3,8 +3,11 @@ from exchange_dialogues_functions import *
 # Inputs
 video1_language, video1_actor, video1_number = "telugu", "Mahesh_Babu", 47
 video2_language, video2_actor, video2_number = "telugu", "Mahesh_Babu", 89
-# generator_model_name = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
-generator_model_name = '/home/voletiv/GitHubRepos/pix2pix/models/DeepLearningImplementations/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
+
+if 'voletiv' in os.getcwd():
+    generator_model_name = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
+elif 'voleti.vikram' in os.getcwd():
+    generator_model_name = '/home/voletiv/GitHubRepos/pix2pix/models/DeepLearningImplementations/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
 
 # Load generator
 generator_model = load_generator(generator_model_name)
@@ -16,6 +19,6 @@ new_video1, new_video2 = exchange_dialogues(generator_model,
                                             verbose=True)
 
 
-imageio.mimwrite('output_filename.mp4', new_video1 , fps=24)
-imageio.mimsave(os.path.join("video1.gif"), new_video1_frames_generated)
+# imageio.mimwrite('output_filename.mp4', new_video1 , fps=24)
+# imageio.mimsave(os.path.join("video1.gif"), new_video1_frames_generated)
 
