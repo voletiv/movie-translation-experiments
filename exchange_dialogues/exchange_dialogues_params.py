142e8644bf575eeda0145d5014c1853b4e2206e1
Vikram Voleti <vikky2904@gmail.com>
2018-03-17 10:45:28 +0530

    GENERATOR_MODEL_NAME

--- exchange_dialogues/exchange_dialogues_params.py
+++ exchange_dialogues/exchange_dialogues_params.py
@@ -10,6 +10,8 @@ import tqdm
 if 'voletiv' in os.getcwd():
     # voletiv
     DATASET_DIR = '/home/voletiv/Datasets/MOVIE_TRANSLATION/'
+    GENERATOR_MODEL_NAME = '/home/voletiv/GitHubRepos/pix2pix/models/DeepLearningImplementations/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
 
 elif 'voleti.vikram' in os.getcwd():
     DATASET_DIR = '/shared/fusor/home/voleti.vikram/MOVIE_TRANSLATION/'
+    GENERATOR_MODEL_NAME = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
