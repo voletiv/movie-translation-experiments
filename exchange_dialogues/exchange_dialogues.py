07cd6e35881f8b8b05eae4f99764ac325c1a83bb
Vikram Voleti <vikky2904@gmail.com>
2018-03-17 12:02:10 +0530

    Bugfix

--- exchange_dialogues/exchange_dialogues.py
+++ exchange_dialogues/exchange_dialogues.py
@@ -6,6 +6,8 @@ from exchange_dialogues_functions import *
 
 if __name__ == '__main__':
 
+    config = MovieTranslationConfig()
+
     parser = argparse.ArgumentParser(description='Exchange dialogues between 2 videos in the MOVIE_TRANSLATION dataset. E.g.: python3 exchange_dialogues.py telugu Mahesh_Babu 47 hindi Aamir_Khan 33')
     parser.add_argument('video1_language', type=str, choices=['english', 'hindi', 'telugu'], help="video1_language: eg. 'telugu'")
     parser.add_argument('video1_actor', type=str, help="video1_actor: eg. 'Mahesh_Babu'")
@@ -13,7 +15,7 @@ if __name__ == '__main__':
     parser.add_argument('video2_language', type=str, choices=['english', 'hindi', 'telugu'], help="video2_language: eg. 'telugu'")
     parser.add_argument('video2_actor', type=str, help="video2_actor: eg. 'Mahesh_Babu'")
     parser.add_argument('video2_number', type=int, help="video2_number: eg. '89'")
-    parser.add_argument('--output_dir', '-o', type=str, default=os.path.join(MOVIE_TRANSLATION_DATASET_DIR, 'exchanged_videos'), help="output_dir to save the videos in: def: '<path/to/MOVIE_TRANSLATION>/exchanged_videos'")
+    parser.add_argument('--output_dir', '-o', type=str, default=os.path.join(config.MOVIE_TRANSLATION_DATASET_DIR, 'exchanged_videos'), help="output_dir to save the videos in: def: '<path/to/MOVIE_TRANSLATION>/exchanged_videos'")
 
     args = parser.parse_args()
 
@@ -22,7 +24,6 @@ if __name__ == '__main__':
     # video2_language, video2_actor, video2_number = "telugu", "Mahesh_Babu", 89
 
     # Load generator
-    config = MovieTranslationConfig()
     generator_model = load_generator(config.GENERATOR_MODEL_NAME)
 
     # Exchange
