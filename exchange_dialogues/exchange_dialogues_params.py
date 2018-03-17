68f9f16f566088971bbe49717908db4454d76f60
Vikram Voleti <vikky2904@gmail.com>
2018-03-17 13:02:38 +0530

    Import params from config file in root

--- exchange_dialogues/exchange_dialogues_params.py
+++ exchange_dialogues/exchange_dialogues_params.py
@@ -1,13 +1,4 @@
-import os
+import sys
 
-class MovieTranslationConfig():
-
-    if 'voletiv' in os.getcwd():
-        # voletiv
-        MOVIE_TRANSLATION_DATASET_DIR = '/home/voletiv/Datasets/MOVIE_TRANSLATION/'
-        GENERATOR_MODEL_NAME = '/home/voletiv/GitHubRepos/pix2pix/models/DeepLearningImplementations/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
-
-    elif 'voleti.vikram' in os.getcwd():
-        # voleti.vikram
-        MOVIE_TRANSLATION_DATASET_DIR = '/shared/fusor/home/voleti.vikram/MOVIE_TRANSLATION/'
-        GENERATOR_MODEL_NAME = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
+sys.path.append('../')
+from config import *
