bc19d7b5bfbb4f99a95419706485b1fae25684d3
Vikram Voleti <vikky2904@gmail.com>
2018-03-17 10:47:04 +0530

    parse_args

--- exchange_dialogues/exchange_dialogues.py
+++ exchange_dialogues/exchange_dialogues.py
@@ -1,24 +1,34 @@
 from exchange_dialogues_functions import *
 
-# Inputs
-video1_language, video1_actor, video1_number = "telugu", "Mahesh_Babu", 47
-video2_language, video2_actor, video2_number = "telugu", "Mahesh_Babu", 89
 
-if 'voleti.vikram' in os.getcwd():
-    generator_model_name = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
-elif 'voletiv' in os.getcwd():
-    generator_model_name = '/home/voletiv/GitHubRepos/pix2pix/models/DeepLearningImplementations/20180314_152941_Mahesh_Babu_black_mouth_polygons/generator_latest.h5'
+if __name__ == '__main__':
 
-# Load generator
-generator_model = load_generator(generator_model_name)
+    parser = argparse.ArgumentParser(description='Train model')
+    parser.add_argument('video1_language', type=str, help="video1_language: eg. 'telugu'")
+    parser.add_argument('video1_actor', type=str,help="video1_actor: eg. 'Mahesh_Babu'")
+    parser.add_argument('video1_number', type=int,help="video1_number: eg. '47'")
+    parser.add_argument('video2_language', type=str,help="video2_language: eg. 'telugu'")
+    parser.add_argument('video2_actor', type=str,help="video2_actor: eg. 'Mahesh_Babu'")
+    parser.add_argument('video2_number', type=int, help="video2_number: eg. '89'")
 
-# Exchange
-new_video1, new_video2 = exchange_dialogues(generator_model,
-                                            video1_language=video1_language, video1_actor=video1_actor, video1_number=video1_number,
-                                            video2_language=video2_language, video2_actor=video2_actor, video2_number=video2_number,
-                                            verbose=True)
+    args = parser.parse_args()
 
+    # Inputs
+    video1_language, video1_actor, video1_number = "telugu", "Mahesh_Babu", 47
+    video2_language, video2_actor, video2_number = "telugu", "Mahesh_Babu", 89
 
-# imageio.mimwrite('output_filename.mp4', new_video1 , fps=24)
-# imageio.mimsave(os.path.join("video1.gif"), new_video1_frames_generated)
+    # Load generator
+    generator_model = load_generator(GENERATOR_MODEL_NAME)
+
+    # Exchange
+    try:
+        new_video1, new_video2 = exchange_dialogues(generator_model,
+                                                    video1_language=video1_language, video1_actor=video1_actor, video1_number=video1_number,
+                                                    video2_language=video2_language, video2_actor=video2_actor, video2_number=video2_number,
+                                                    verbose=True)
+    except ValueError as err:
+        print(err)
+
+    # imageio.mimwrite('output_filename.mp4', new_video1 , fps=24)
+    # imageio.mimsave(os.path.join("video1.gif"), new_video1_frames_generated)
 
