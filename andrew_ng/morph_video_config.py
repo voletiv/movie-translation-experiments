fbf5bb3b2b8821b82977f48e745613c7b807c39c
Vikram Voleti <vikky2904@gmail.com>
2018-05-04 10:54:50 +0530

    New generator model of andrew_ng_clean_small

--- andrew_ng/morph_video_config.py
+++ andrew_ng/morph_video_config.py
@@ -1,4 +1,6 @@
-generator_model = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180414_015206_andrew_ng/generator_latest.h5'
+# generator_model = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180414_015206_andrew_ng/generator_latest.h5'
+generator_model = '/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180503_233616_andrew_ng_small/generator_latest.h5'
+
 dataset_dir = '/shared/fusor/home/voleti.vikram/ANDREW_NG'
 person = 'andrew_ng'
 
