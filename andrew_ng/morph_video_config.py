f8bcd5f076c07c7cd91e51515a45185c563a0329
Vikram Voleti <vikky2904@gmail.com>
2018-05-08 08:05:33 +0530

    Updated

--- andrew_ng/morph_video_config.py
+++ andrew_ng/morph_video_config.py
@@ -1,5 +1,7 @@
 # generator_model = '/shared/fusor/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180414_015206_andrew_ng/generator_latest.h5'
 generator_model = '/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180504_111328_andrew_ng_small/generator_latest.h5'
+# generator_model = '/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180504_111328_andrew_ng_small/gen_weights_epoch00509_genLoss0.0284.h5'
+# generator_model = '/home/voleti.vikram/DeepLearningImplementations/pix2pix/models/20180414_015206_andrew_ng/gen_weights_epoch01249_discLoss0.3411_genTotL2.2075_genL1L0.0306_genLogL1.9018.h5'
 
 dataset_dir = '/shared/fusor/home/voleti.vikram/ANDREW_NG'
 person = 'andrew_ng'
@@ -7,3 +9,5 @@ person = 'andrew_ng'
 ANDREW_NG_VIDEO_FPS = 30
 generated_lip_landmarks_fps = 25
 
+LIP_CLUSTERS_FILE = '/home/voleti.vikram/movie-translation-experiments/lip_landmarks_experiments/gmm_clusters/gmm_cluster_centers.npy'
+CLOSED_LIP_CLUSTER_INDEX = 2
