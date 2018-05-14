74dc8340c341d6189b72733a432d65939b0b7174
Vikram Voleti <vikky2904@gmail.com>
2018-05-14 22:03:05 +0530

    Made more efficient using cv2 video readFrames indexing

--- andrew_ng/make_dataset_images_from_landmarks.py
+++ andrew_ng/make_dataset_images_from_landmarks.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import imageio
 import os
@@ -14,10 +15,13 @@ import utils
 def make_combined_face_with_bmp(video_file_name, output_dir_name, align=False):
 
     # Read video
+    cap = cv2.VideoCapture(video_file_name)
+    """
     video_reader = imageio.get_reader(video_file_name)
     frames = []
     for frame in tqdm.tqdm(video_reader):
         frames.append(frame)
+    """
 
     if os.path.basename(video_file_name)[2] == '.':
         video_file_name = "CV_" + '_'.join('_'.join(os.path.splitext(os.path.basename(video_file_name))[0].split('.')).split(' '))
@@ -26,32 +30,38 @@ def make_combined_face_with_bmp(video_file_name, output_dir_name, align=False):
     landmarks, frames_with_no_landmarks = read_video_landmarks(video_file_name=video_file_name)
 
     # MAKE FACE WITH BMP
-    for i, (frame, landmarks_in_frame, frame_with_no_landmarks) in tqdm.tqdm(enumerate(zip(frames, landmarks, frames_with_no_landmarks)),
-                                                                             total=len(frames)):
+    for i, (landmarks_in_frame, frame_with_no_landmarks) in tqdm.tqdm(enumerate(zip(landmarks, frames_with_no_landmarks)),
+                                                                             total=len(landmarks)):
         if not frame_with_no_landmarks:
 
-            # Extract face_rect, landmarks
-            face_square_expanded_resized, \
-                landmarks_in_face_square_expanded_resized, \
-                face_rect_in_frame, face_original_size = utils.get_square_expand_resize_face_and_modify_landmarks(np.array(frame),
-                                                                                                                  landmarks_in_frame,
-                                                                                                                  resize_to_shape=(256, 256),
-                                                                                                                  face_square_expanded_resized=True)
-            # Make black mouth and lips polygons
-            mouth_landmarks_in_face = landmarks_in_face_square_expanded_resized[48:68]
-            face_square_expanded_resized_with_bmp = utils.make_black_mouth_and_lips_polygons(face_square_expanded_resized,
-                                                                                             mouth_landmarks_in_face,
-                                                                                             align=align)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+            ret, frame = cap.read()
 
-            # Make combined face
-            face_combined = np.hstack((face_square_expanded_resized, face_square_expanded_resized_with_bmp))
-
-            # Save combined face
-            video_base_name = os.path.splitext(os.path.basename(video_file_name))[0]
-            image_name = os.path.join(output_dir_name, video_base_name, video_base_name + "_combined_faces_frame_{0:05d}.png".format(i))
-            if not os.path.exists(os.path.dirname(image_name)):
-                os.makedirs(os.path.dirname(image_name))
-            imageio.imwrite(image_name, face_combined)
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+                # Extract face_rect, landmarks
+                face_square_expanded_resized, \
+                    landmarks_in_face_square_expanded_resized, \
+                    face_rect_in_frame, face_original_size = utils.get_square_expand_resize_face_and_modify_landmarks(np.array(frame),
+                                                                                                                      landmarks_in_frame,
+                                                                                                                      resize_to_shape=(256, 256),
+                                                                                                                      face_square_expanded_resized=True)
+                # Make black mouth and lips polygons
+                mouth_landmarks_in_face = landmarks_in_face_square_expanded_resized[48:68]
+                face_square_expanded_resized_with_bmp = utils.make_black_mouth_and_lips_polygons(face_square_expanded_resized,
+                                                                                                 mouth_landmarks_in_face,
+                                                                                                 align=align)
+    
+                # Make combined face
+                face_combined = np.hstack((face_square_expanded_resized, face_square_expanded_resized_with_bmp))
+    
+                # Save combined face
+                video_base_name = os.path.splitext(os.path.basename(video_file_name))[0]
+                image_name = os.path.join(output_dir_name, video_base_name, video_base_name + "_combined_faces_frame_{0:05d}.png".format(i))
+                if not os.path.exists(os.path.dirname(image_name)):
+                    os.makedirs(os.path.dirname(image_name))
+                imageio.imwrite(image_name, face_combined)
 
 
 if __name__ == '__main__':
